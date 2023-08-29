5fdc56e6a69a5c4eb22c5f8ba7a723e987968ea7
HappyGames <harkaransingh528@gmail.com>
2023-08-29 22:12:43 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import { AiOutlineArrowRight } from 'react-icons/ai';
-import Logo from '../assets/logo4.webp';
+import Logo from '../assets/S12.gif';
 import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { RxCross2 } from 'react-icons/rx';
+import { RiMenu2Fill } from 'react-icons/ri';
+import { SlMagnifier } from 'react-icons/sl';
+import { AiOutlineCamera, AiOutlineHeart } from 'react-icons/ai';
+import { BsBag, BsPerson } from 'react-icons/bs';
+import { RxCross1 } from 'react-icons/rx';
+
+
+
+
 
 const Navbar = () => {
 
   const app = useRef();
 
   const [menu, setMenu] = useState(false);
-  
-  const handleclick = () => {
+
+  const handleClick = () => {
 
     setMenu(!menu)
   }
@@ -25,13 +33,13 @@ const Navbar = () => {
     let ctx = gsap.context(() => {
 
 
-        gsap.from(".navbar", {
+        gsap.from(app.current, {
           y: -100,
-          duration: 1,
+          duration: 0.5,
           delay: 0.5,
         })
-      
-      
+
+
 
     }, app)
 
@@ -40,45 +48,45 @@ const Navbar = () => {
   }, [])
 
   
+
+
   return (
-    <div ref={app} className='w-full uppercase text-[#E8E8E8] flex flex-row justify-between items-center px-[10%] py-4'>
+    <>
+      <div ref={app} className='navbar w-full h-[100px] flex flex-row justify-between items-center px-12 bg-white'>
 
-      <div className='navbar w-[100%] h-[100%] flex flex-row justify-between items-center'>
+        <div >
+          <RiMenu2Fill className='manu-icon' onClick={handleClick} size={30} />
+        </div>
 
-      
-      <div className='flex flex-row justify-between items-center gap-24'>
-        <img className='w-[90px] h-[90px]' src={Logo} alt="" />
+        <img className='w-[170px] h-[100px]' src={Logo} alt="" />
 
-
-
-        <div className={menu ? 'mobile-show flex flex-row justify-between items-center gap-[24vw]' : 'mobile flex flex-row justify-between items-center gap-[24vw]'}>
-        <img className='mobile-image w-[90px] h-[90px]' src={Logo} alt="" />
-        
-          <div className='cross md:hidden'>
-            {/* <RxCross2 onClick={handleclick} size={30} color='black' /> */}
-          </div>
-          <div className='mobile-items flex flex-col first-letter md:flex-row gap-10 justify-between items-center'>
-            <Link to={"#"}><h1 className='nav-items'>Home</h1></Link>
-            <Link to={"#"}><h1 className='nav-items'>About</h1></Link>
-            <Link to={"#"}><h1 className='nav-items'>Contact</h1></Link>
-            <Link to={"#"}><h1 className='nav-items'>Products</h1></Link>
-          </div>
-
-
-          <button  className='bg-gradient-to-r from-[#F4256A] to-[#FE6412]  flex gap-2 items-center py-3 px-7 rounded-sm'>Buy Products
-            <AiOutlineArrowRight  size={20} />
-          </button>
+        <div className="icons flex flex-row justify-between items-center gap-2">
+          <BsPerson size={25} />
+          <SlMagnifier size={25} />
+          <AiOutlineCamera size={25} />
+          <AiOutlineHeart size={25} />
+          <BsBag size={25} />
+        </div>
 
         </div>
-      </div>
 
+        <div className= {menu ? "absolute sidebar-show w-[50%] h-[100vh] bg-white text-center" : 
+        "absolute sidebar-hide w-[50%] h-[100vh] bg-white text-center "}>
+          <div className='absolute md:top-[4%] md:left-[90%] left-[80%]'>
+            <RxCross1 onClick={handleClick} size={30} />
+          </div>
+          
+          <div className="nav-items flex flex-col justify-between items-center gap-8 mt-[18%]">
+            <Link to="#"><h1>HOME</h1></Link>
+            <Link to="#"><h1>ABOUT</h1></Link>
+            <Link to="#"><h1>CONTACT</h1></Link>
+            <Link to="#"><h1>PRODUCTS</h1></Link>
+          </div>
 
+        </div>
 
-      <div className='buger md:hidden'>
-        <GiHamburgerMenu onClick={handleclick} size={30} />
-      </div>
-      </div>
-    </div>
+      
+    </>
   )
 }
 
